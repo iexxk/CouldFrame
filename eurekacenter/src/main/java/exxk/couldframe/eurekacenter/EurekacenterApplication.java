3728802986af9f1acb050fec1bb5f4d23671c7dc
xuan <liangx@3sreform.com>
2018-06-21 15:51:09 +0800

    添加配置中心

--- eurekacenter/src/main/java/exxk/couldframe/eurekacenter/EurekacenterApplication.java
+++ eurekacenter/src/main/java/exxk/couldframe/eurekacenter/EurekacenterApplication.java
@@ -1,15 +1,17 @@
 package exxk.couldframe.eurekacenter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-@EnableEurekaClient
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekacenterApplication {
-
+    @Value("${test}")  //调用配置中心的配
+   static String test;
     public static void main(String[] args) {
         SpringApplication.run(EurekacenterApplication.class, args);
+        System.out.print(""+test);
     }
 }
