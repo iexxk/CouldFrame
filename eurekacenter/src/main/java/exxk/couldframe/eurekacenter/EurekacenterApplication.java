7c45906e753ff57c51f389f3bcb6813d6fdb87fc
xuan <liangx@3sreform.com>
2018-07-24 15:16:43 +0800

    修改为高可用配置中心

--- eurekacenter/src/main/java/exxk/couldframe/eurekacenter/EurekacenterApplication.java
+++ eurekacenter/src/main/java/exxk/couldframe/eurekacenter/EurekacenterApplication.java
@@ -1,17 +1,12 @@
 package exxk.couldframe.eurekacenter;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekacenterApplication {
-    @Value("${test}")  //调用配置中心的配
-   static String test;
     public static void main(String[] args) {
         SpringApplication.run(EurekacenterApplication.class, args);
-        System.out.print(""+test);
     }
 }
