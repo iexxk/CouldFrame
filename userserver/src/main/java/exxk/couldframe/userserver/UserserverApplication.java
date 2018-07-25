964b27b9860c970115ee77e3aaad3bc4a1e8b6e7
xuan <liangx@3sreform.com>
2018-07-25 15:44:16 +0800

    添加用户服务

---
+++ userserver/src/main/java/exxk/couldframe/userserver/UserserverApplication.java
@@ -0,0 +1,14 @@
+package exxk.couldframe.userserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class UserserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserserverApplication.class, args);
+    }
+}
